6e32f2e345be030676fcbbdbeab83abe8056970c
ישראל <israelb300790@gmail.com>
2026-05-10 23:17:29 +0300

    Add project files.

---
+++ home_work_001/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic;
+
+namespace home_work_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.כתוב תוכנית שמבקשת מהמשתמש את שמו ומדפיסה:
+            //Console.WriteLine("enter a name:");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"my name is: {name}");
+
+            //2.
+            //כתוב תוכנית שמקבלת שני מספרים מהמשתמש ומדפיסה את הסכום שלהם
+            //Console.WriteLine("enter a first number :");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("enter a secund number:");
+            //int num2 = int.Parse(Console.ReadLine());
+            //int result = num1 + num2;
+            //Console.WriteLine($"result is : {num1} + {num2} = {result}");
+
+
+        }
+    }
+}
