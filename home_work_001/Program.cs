ddd18b360c99665fd708bb73a5466414ad259702
ישראל <israelb300790@gmail.com>
2026-05-13 22:18:00 +0300

    update home work

--- home_work_001/Program.cs
+++ home_work_001/Program.cs
@@ -37,17 +37,83 @@ namespace home_work_001
             //5.מציאת המספר הגדול מבין שניים
             //קבל שני מספרים והדפס מי הגדול יותר.
 
-            Console.WriteLine("enter a first number:");
-            int num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("enter a secund number:");
-            int num2 = int.Parse(Console.ReadLine());
-            if (num1 > num2)
+            //Console.WriteLine("enter a first number:");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("enter a secund number:");
+            //int num2 = int.Parse(Console.ReadLine());
+            //if (num1 > num2)
+            //    Console.WriteLine($"{num1} is bigger");
+            //else if (num2 > num1)
+            //    Console.WriteLine($"{num2} is bigger");
+            //else
+            //    Console.WriteLine("Both numbers are equal");
+
+            //6 .חישוב ממוצע של 3 מספרים
+            //קבל מהמשתמש שלושה מספרים והדפס את הממוצע שלהם.
+
+            //Console.WriteLine("enter a first number:");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("enter a second number:");
+            //int num2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("enter a thred number:");
+            //int num3 = int.Parse(Console.ReadLine());
+            //int result = num1 + num2 + num3;
+            //Console.WriteLine($"result avg is :{result/3}");
+
+            //7. בדיקה אם מספר חיובי, שלילי או אפס
+            //קבל מספר מהמשתמש והדפס:
+            //"Positive" אם הוא חיובי
+            //"Negative" אם הוא שלילי
+            //"Zero" אם הוא אפס
+
+            //Console.WriteLine("enter a number im cheek number");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num > 0)
+            //    Console.WriteLine("number is Positive");
+            //else if (num < 0)
+            //    Console.WriteLine("number is Negative");
+            //else
+            //    Console.WriteLine("number is Zero");
+
+            ////8.הדפסת כל המספרים הזוגיים מ‑1 עד מספר שהמשתמש נותן
+            ////המשתמש מכניס מספר N, ואתה מדפיס את כל המספרים הזוגיים מ‑1 עד N.
+
+            //int num = int.Parse(Console.ReadLine());
+            //for (int i = 1; i < num; i++)
+            //{
+            //    if (i %2 == 0)
+            //    {
+            //        Console.WriteLine($"number zoggi:{i}");
+            //    }
+            //}
+
+            //9.קבלת שם וגיל — והדפסת משפט מותאם
+            //קבל שם וגיל מהמשתמש.
+            //אם הגיל קטן מ‑18 — הדפס: "Hello {name}, you are a minor"
+            //אחרת — "Hello {name}, you are an adult"
+
+            //Console.WriteLine("enter a name:");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("enter age:");
+            //int age = int.Parse(Console.ReadLine());
+            //if (age < 18)
+            //{
+            //    Console.WriteLine($"Hello {name}, you are a minor");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Hello {name}, you are an adult");
+            //}
+
+            //10.   לולאה שמבקשת מספרים עד שהמשתמש מכניס 0
+            //בכל פעם שהמשתמש מכניס מספר — הדפס אותו.
+            //כאשר הוא מכניס 0 — עצור את התוכנית.
+
+            int num = int.Parse(Console.ReadLine());
+            while (num > 0)
             {
-                Console.WriteLine("the number first is big number");
-            }
-            else
-            {
-                Console.WriteLine("the number secund is big");
+                num = int.Parse(Console.ReadLine());
+                Console.WriteLine(num);
             }
         }
     }
